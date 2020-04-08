9637af5d376bfca510fc62d897dad6c46a93dc69
makbari99 <makbari@luc.edu>
2020-04-08 16:51:57 -0500

    made Main

--- CodeMath.java
+++ CodeMath.java
@@ -1,3 +1,12 @@
+import java.util.List;
+
 public class CodeMath {
 
+  public static String CodeMath(final List<String> letter) {
+    String temp = "";
+    for (int i = 0; i < letter.size(); i++) {
+      //temp = letter.get(i).toCharArray();
+    }
+    //return CodeMath(temp);
+  }
 }
--- Main.java
+++ Main.java
@@ -7,13 +7,16 @@ public class Main {
     public static void main(final String[] args) {
 
         //read data
-        final Scanner console = new Scanner(System.in);
+        System.out.println("Enter your message here:");
+        final Scanner console = new Scanner(System.in); //look at scanner API
         final List<String> letter = new ArrayList<>();
-        while (console.hasNext()) {
+        while (console.hasNext()) { //try has
             final String value = console.next();
             letter.add(value);
 
         }
         System.out.println("Your message is: " + letter) ;
+        final String result = CodeMath.CodeMath(letter);
     }
+
 }
