987836373f1951190987309f4352a918f3f6035b
makbari99 <makbari@luc.edu>
2020-04-25 21:15:47 -0500

    updated encryption message and the program is working

--- Main.java
+++ Main.java
@@ -6,14 +6,35 @@ public class Main {
 
     public static void main(final String[] args) {
 
-        //read data
+        System.out.println("Enter your message:");
         final Scanner console = new Scanner(System.in);
-        final List<String> letter = new ArrayList<>();
-        while (console.hasNext()) {
-            final String value = console.next();
-            letter.add(value);
+        String input = console.nextLine();
+        System.out.println("Enter your secret code:");
+        int code = console.nextInt();
+        String result = "";
+        StringBuilder sb = new StringBuilder(result);
+        char start = input.charAt(0);
+        char end = input.charAt(input.length() - 1);
 
+        if (start == ' ' && end == ' ') {
+            for (int i = 1; i < input.length() - 1; i++) {
+                char c = input.charAt(i);
+                c += (code);
+                sb.insert((i - 1), c);
+            }
+        } else {
+            for (int i = 0; i < input.length(); i++) {
+                char c = input.charAt(i);
+                c -= (code);
+                sb.insert(i, c);
+            }
         }
-        System.out.println("Your message is: " + letter) ;
+        if (start == ' ' && end == ' ') {
+            System.out.println("Your decoded message is:");
+            System.out.println(sb);
+        } else {
+            System.out.println("Your encoded message is:");
+            System.out.println(" " + sb + " ");
+        }
     }
 }
