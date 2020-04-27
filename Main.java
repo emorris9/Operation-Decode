c5247634e7f8ed51b810e568267349051caf29df
emorris9 <54684816+emorris9@users.noreply.github.com>
2020-04-26 22:26:58 -0400

    Add files via upload
    Main with an updated encryption substitution cipher

--- Main.java
+++ Main.java
@@ -15,17 +15,18 @@ public class Main {
         StringBuilder sb = new StringBuilder(result);
         char start = input.charAt(0);
         char end = input.charAt(input.length() - 1);
+        int temp = (code + 24) % 16 * 3 - 5;
 
         if (start == ' ' && end == ' ') {
             for (int i = 1; i < input.length() - 1; i++) {
                 char c = input.charAt(i);
-                c += (code);
+                c += (temp);
                 sb.insert((i - 1), c);
             }
         } else {
             for (int i = 0; i < input.length(); i++) {
                 char c = input.charAt(i);
-                c -= (code);
+                c -= (temp);
                 sb.insert(i, c);
             }
         }
